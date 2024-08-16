8e6ddc7ce12e8ec75b223b4d1c9d2b51783665ee
harsita <131437586+harsita-keerthi@users.noreply.github.com>
2024-08-15 17:08:02 -0700

    navbar menu overlay for mobile app

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState} from 'react'
 import Link from 'next/link'
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
     {
@@ -17,12 +18,12 @@ const navLinks = [
         title: "Contact",
         path: "#contact",
     },
-]
+];
 
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
     return (
-        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
+        <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
             <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link 
                     href={"/"} 
@@ -51,6 +52,7 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
+            {navbarOpen ? <MenuOverlay links={navLinks}/> : null}
         </nav>
     )
 }
